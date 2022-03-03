f3ac61e269ec77fbbced011fd696d713c4a868a0
Yana Stoyanidu <yana955@mail.ru>
2022-03-03 20:47:00 +0300

    add adaptive layout

--- src/app.js
+++ src/app.js
@@ -1,9 +1,52 @@
 import './styles/style.scss';
 import * as $ from 'jquery';
 
-var btn = $('#button');
+const animItems = document.querySelectorAll('.anim-items');
 
-$(window).scroll(function() {
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('active');
+      }else{
+        if (!animItem.classList.contains('anim-no-hide')){
+          animItem.classList.remove('active');
+        }
+      }
+    }
+  }
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+      scrollTop = window.scrollX || document.documentElement.scrollTop;
+    return {
+      top: rect.top + scrollTop,
+      left: rect.left + scrollLeft
+    };
+  }
+
+setTimeout(() => {
+  animOnScroll();
+}, 300);
+  
+}
+
+
+var btn = $('#buttonUp');
+
+$(window).scroll(function () {
   if ($(window).scrollTop() > 300) {
     btn.addClass('show');
   } else {
@@ -11,7 +54,17 @@ $(window).scroll(function() {
   }
 });
 
-btn.on('click', function(e) {
+btn.on('click', function (e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+  $('html, body').animate({
+    scrollTop: 0
+  }, '300');
+});
+
+const navToggle = $("#navToggle");
+const nav = $("#nav");
+
+navToggle.on("click", function (e) {
+    e.preventDefault();
+    nav.toggleClass("show");
 });
